23cecfeb1cb25d67aeb272b663637a3d91abc686
Jan Illetschko <jan.illetschko@3kraft.com>
2018-11-27 12:08:52 +0100

    fixes eyes and tongue again

--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,22 @@
 module.exports = {
-  /**
-  	 * Parse arguments.
-  	 *
-  	 * @param  {string} text
-  	 * @param  {string} argumentName
-  	 * @return {string|null}
-  	 */
-  parseArguments: function(text, argumentName) {
-    var arguments = text.match(/[^[\]]+(?=])/g);
+    /**
+     * Parse arguments.
+     *
+     * @param  {string} text
+     * @param  {string} argumentName
+     * @return {string|null}
+     */
+    parseArguments: function (text, argumentName) {
+        var arguments = text.match(/[^[\]]+(?=])/g);
 
-    if (!arguments) {
-      return;
-    }
+        if (!arguments) {
+            return;
+        }
 
-    return arguments.map(function(arg) {
-      if (arg.split(' ')[0] === argumentName) {
-        return arg.substring(argumentName.length).trim();
-      }
-    });
-  },
+        const arg =  arguments.find(function (arg) {
+            return arg.split(' ')[0] === argumentName;
+        });
+
+        return arg && arg.substring(argumentName.length).trim();
+    },
 };
